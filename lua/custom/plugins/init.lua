02efe2a5f7b509c247b42fa8f72b200a1598ceae
Pierre Lessard <plessard@konradgroup.com>
2025-05-07 15:15:40 -0400

    basic config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Add the auto-save plugin
+  {
+    'pocco81/auto-save.nvim',
+    config = function()
+      require('auto-save').setup {
+        enabled = true,
+        execution_message = { enabled = false },
+        trigger_events = { 'InsertLeave', 'TextChanged' },
+        condition = function(buf)
+          -- Disable autosave for init.lua or any other specific file
+          local filename = vim.api.nvim_buf_get_name(buf)
+          return not filename:match 'init.lua$'
+        end,
+      }
+    end,
+  },
+}
